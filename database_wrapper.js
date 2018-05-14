a0ed5f38175df8b81c2c0abc18b976815149a84b
Seth Childers <sethchilders92@gmail.com>
2018-05-14 10:12:06 -0600

    Added stub functions for CRUD methods in database_wrapper.js

--- database_wrapper.js
+++ database_wrapper.js
@@ -24,3 +24,75 @@ function getCourse(courseID) {
 function getReport(username) {
 
 }
+
+/**
+ * Updates a specific user's information in the database.
+ * @param {string} - The user's name
+ * @returns {object} - The user
+ */
+function updateUser(username) {
+
+}
+
+/**
+ * Updates a specific course's information in the database.
+ * @param {integer} - The course's ID
+ * @returns {object} - The course
+ */
+function updateCourse(courseID) {
+
+}
+
+/**
+ * Creates a new user in the database.
+ * @param {string} - The user's name
+ * @returns {object} - The user
+ */
+function newUser(username) {
+
+}
+
+/**
+ * Creates a new course in the database.
+ * @param {integer} - The course's ID
+ * @returns {object} - The course
+ */
+function newCourse(courseID) {
+
+}
+
+/**
+ * Creates a new report in the database.
+ * @param {integer} - The report's ID
+ * @returns {object} - The report
+ */
+function newReport(username) {
+
+}
+
+/**
+ * Deletes a specific user from the database.
+ * @param {string} - The user's name
+ * @returns {object} - The user
+ */
+function deleteUser(username) {
+
+}
+
+/**
+ * Deletes a specific course from the database.
+ * @param {integer} - The course's ID
+ * @returns {object} - The course
+ */
+function deleteCourse(courseID) {
+
+}
+
+/**
+ * Deletes a specific report from the database.
+ * @param {integer} - The report's ID
+ * @returns {object} - The report
+ */
+function deleteReport(username) {
+
+}
