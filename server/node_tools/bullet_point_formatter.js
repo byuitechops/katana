00c97412150924dc8ca4609e8900e18d4cea63c9
Wilson <kilakal@byui.edu>
2018-07-17 16:03:23 -0600

    fo rpull

--- server/node_tools/bullet_point_formatter.js
+++ server/node_tools/bullet_point_formatter.js
@@ -10,66 +10,69 @@ const cheerio = require('cheerio');
 
 // This needs to be re-written so that Items with only 1 OL do not get discovered
 function discover(canvasItem, issueItem, options) {
-    if (canvasItem.getHtml() === null) return;
-    var $ = cheerio.load(canvasItem.getHtml());
-    var ols = $('ol').get();
-    var types = {
-        0: '1',
-        1: 'a',
-        2: 'i'
-    };
+  if (canvasItem.getHtml() === null) return;
+  let $ = cheerio.load(canvasItem.getHtml());
+  let ols = $('ol').get();
+  let types = {
+    0: '1',
+    1: 'a',
+    2: 'i'
+  };
+  let html = {
+    currentHtml:
+  }
 
-    function getChildren(ol, olsToFix, oldTypes, newTypes, i) {
-        if (i > 2) i = 0;
-        oldTypes.push($(ol).attr('type') ? 'type' : 'Not Defined');
-        newTypes.push(types[i]);
-        olsToFix.push({
-            type: types[i],
-            ol: ol
+  function getChildren(ol, olsToFix, oldTypes, newTypes, i) {
+    if (i > 2) i = 0;
+    oldTypes.push($(ol).attr('type') ? 'type' : 'Not Defined');
+    newTypes.push(types[i]);
+    olsToFix.push({
+      type: types[i],
+      ol: ol
+    });
+    i++;
+    ol.children.forEach(child => {
+      if (child.name === 'ol') {
+        getChildren(child, olsToFix, oldTypes, newTypes, i);
+      } else if (child.name === 'li') {
+        child.children.forEach(item => {
+          if (item.name === 'ol') {
+            getChildren(item, olsToFix, oldTypes, newTypes, i);
+          }
         });
-        i++;
-        ol.children.forEach(child => {
-            if (child.name === 'ol') {
-                getChildren(child, olsToFix, oldTypes, newTypes, i);
-            } else if (child.name === 'li') {
-                child.children.forEach(item => {
-                    if (item.name === 'ol') {
-                        getChildren(item, olsToFix, oldTypes, newTypes, i);
-                    }
-                });
-            }
-        });
-    }
+      }
+    });
+  }
 
-    function getNewOl(olsToFix, newTypes) {
-        olsToFix.forEach((olToFix, i) => {
-            $(olToFix.ol).attr('type', newTypes[i]);
-            console.log(olToFix.ol);
-        });
-        return olsToFix[0].ol;
-    }
+  function getNewOl(olsToFix, newTypes) {
+    olsToFix.forEach((olToFix, i) => {
+      $(olToFix.ol).attr('type', newTypes[i]);
+      console.log(olToFix.ol);
+    });
+    return olsToFix[0].ol;
+  }
 
-    /* If there aren't any ordered lists in the item then return */
-    if (ols.length === 0) return;
-    ols.forEach(ol => {
-        if (ol.parent.name !== 'ol' && ol.parent.name !== 'li') {
-            var attribute = $(ol).attr('type') ? 'type' : 'N/A';
-            var olsToFix = [];
-            var oldTypes = [];
-            var newTypes = [];
-            var oldOl = String($(ol).html());
-            var newOl;
+  /* If there aren't any ordered lists in the item then return */
+  if (ols.length === 0) return;
+  ols.forEach(ol => {
+    if (ol.parent.name !== 'ol' && ol.parent.name !== 'li') {
+      let attribute = $(ol).attr('type') ? 'type' : 'N/A';
+      let olsToFix = [];
+      let oldTypes = [];
+      let newTypes = [];
+      let oldOl = String($(ol).html());
+      let newOl;
 
 
-            //Get the parents Children
+      //Get the parents Children
 
-            getChildren(ol, olsToFix, oldTypes, newTypes, 0);
+      getChildren(ol, olsToFix, oldTypes, newTypes, 0);
 
-            // Check if the OL has an ordered list(s) as a child, if it does add an issue item, else do not.
-            if (olsToFix.length > 1) {
-                newOl = getNewOl(olsToFix, newTypes);
-                let title = 'Ordered List Type Incorrect';
-                let display = `
+      // Check if the OL has an ordered list(s) as a child, if it does add an issue item, else do not.
+      if (olsToFix.length > 1) {
+        newOl = getNewOl(olsToFix, newTypes);
+        let title = 'Ordered List Type Incorrect';
+        let display = `
                 <div>This ordered list doesn't contain the correct type. This will update it so it has the correct type.</div>
                 <h3>old ordered list</h3>
                     <div class="code-block">
@@ -80,16 +83,16 @@ function discover(canvasItem, issueItem, options) {
                     <ol type="1">${$(newOl).html()}</ol>
                     </div>
                 `;
-                let details = {
-                    tag: ol.tagName,
-                    attribute,
-                    oldTypes,
-                    newTypes
-                };
-                issueItem.newIssue(title, display, details);
-            }
-        }
-    });
+        let details = {
+          tag: ol.tagName,
+          attribute,
+          oldTypes,
+          newTypes
+        };
+        issueItem.newIssue(title, display, details);
+      }
+    }
+  });
 }
 
 /*****************************************************************
@@ -100,46 +103,52 @@ function discover(canvasItem, issueItem, options) {
  * @returns {array} fixedIssues - All issues discovered.
  *****************************************************************/
 function fix(canvasItem, issueItem, options) {
-    return new Promise(async (resolve, reject) => {
-        try {
-            var $ = cheerio.load(canvasItem.getHtml());
-            issueItem.issues.forEach(issue => {
-                if (issue.status === 'approved') {
-                    let link = $(`${issue.details.tag}[${issue.details.attribute}="${issue.details.oldUrl}"]`).first();
-                    if (link) {
-                        let newUrl = $(link).attr(issue.details.attribute);
-                        newUrl = newUrl.replace(/\/file\//i, '/integ/gen/');
-                        newUrl = newUrl.replace(/\/\d+\//i, '/0/');
-                        $(link).attr(issue.details.attribute, newUrl);
-                    }
-                    issue.status = 'fixed';
-                }
-            });
-            canvasItem.setHtml($.html());
-            await canvasItem.update();
-            resolve();
-        } catch (e) {
-            issueItem.issues[0].status = 'failed';
-            reject(e);
+  return new Promise(async (resolve, reject) => {
+    try {
+      let $ = cheerio.load(canvasItem.getHtml());
+      issueItem.issues.forEach(issue => {
+        if (issue.status === 'approved') {
+          let link = $(`${issue.details.tag}[${issue.details.attribute}="${issue.details.oldUrl}"]`).first();
+          if (link) {
+            let newUrl = $(link).attr(issue.details.attribute);
+            newUrl = newUrl.replace(/\/file\//i, '/integ/gen/');
+            newUrl = newUrl.replace(/\/\d+\//i, '/0/');
+            $(link).attr(issue.details.attribute, newUrl);
+          }
+          issue.status = 'fixed';
         }
-    });
+      });
+      canvasItem.setHtml($.html());
+      await canvasItem.update();
+      resolve();
+    } catch (e) {
+      issueItem.issues[0].status = 'failed';
+      reject(e);
+    }
+  });
 }
 
 module.exports = {
-    discover,
-    fix,
-    id: 'bullet_point_formatter',
-    title: 'Update Ordered Lists',
-    description: 'Fixes the styling of ordered lists.',
-    icon: 'list',
-    categories: [
-        'pages',
-        'assignments',
-        'discussions',
-        'quizzes',
-        'quizQuestions'
-    ],
-    toolCategory: 'html',
-    discoverOptions: [],
-    fixOptions: [],
+  discover,
+  fix,
+  id: 'bullet_point_formatter',
+  title: 'Update Ordered Lists',
+  description: 'Fixes the styling of ordered lists.',
+  icon: 'list',
+  toolType: 'fix',
+  toolCategory: 'html',
+  categories: [
+    'pages',
+    'assignments',
+    'discussions',
+    'quizzes',
+    'quizQuestions'
+  ],
+  discoverOptions: [],
+  fixOptions: [],
+  editorTabs: [{
+    title: 'HTML',
+    htmlKey: 'currentHtml',
+    readOnly: true
+  }]
 };
