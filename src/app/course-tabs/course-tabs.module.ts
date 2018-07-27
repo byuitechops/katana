81c1534d0e1488dc4f9ec9dfd1197bcbb8941f42
Zachary  Williams <wil11063@byui.edu>
2018-07-27 14:42:47 -0600

    Update course-tabs.module.ts

--- src/app/course-tabs/course-tabs.module.ts
+++ src/app/course-tabs/course-tabs.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterializeModule } from 'angular2-materialize';
 import { RouterModule } from '@angular/router';
+
 import { CourseSelectionComponent } from './course-selection/course-selection.component';
 import { CourseTabsComponent } from './course-tabs/course-tabs.component';
 
