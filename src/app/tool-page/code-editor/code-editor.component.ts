a5b86fa48aab21245d22d422d1fc2200af292988
Joshua McKinney <mckinneyj@byui.edu>
2020-01-28 16:45:47 -0700

    made _tabs public and added new terms

--- src/app/tool-page/code-editor/code-editor.component.ts
+++ src/app/tool-page/code-editor/code-editor.component.ts
@@ -62,7 +62,7 @@ export class CodeEditorComponent implements OnInit {
      * This helps with those reference issues. This is set during the "ngOnInit"
      * method.
      */
-    private _tabs: any[] = [];
+    public _tabs: any[] = [];
 
     /**
      * Constructor
